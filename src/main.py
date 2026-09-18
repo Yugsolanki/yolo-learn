086f7ebd05f492e28b5e9a4d3a609177bf7a5749
Yug Solanki <yugsolanki15@gmail.com>
2026-09-18 11:18:56 +0530

    feat(src): implement video stream processing for real-time YOLO predictions

--- src/main.py
+++ src/main.py
@@ -3,8 +3,34 @@ import cv2
 from PIL import Image
 import os
 
-DATASET_PATH = os.path.join(os.path.dirname(__file__), "../data")
-
 model = YOLO(model="yolo26n.pt")
 
-results = model.predict(source="0", save=True, save_txt=True, conf=0.75, device="0", classes=[74])
+stream_url = 0
+cap = cv2.VideoCapture(stream_url)
+
+if not cap.isOpened():
+    print("Error: could not open video stream")
+    exit()
+    
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Stream disconnected or ended")
+        break
+    
+    results = model.predict(source=frame, conf=0.5, verbose=False, classes=[74])
+    
+    for box in results[0].boxes:
+        x1, y1, x2, y2 = box.xyxy[0].tolist()
+        conf = box.conf[0].item()
+        
+        # Downstream logic to read time
+    
+    annotated_frame = results[0].plot()
+    cv2.imshow("CCTV Clock Detection", annotated_frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
